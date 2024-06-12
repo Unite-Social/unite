29dd52e3e0aa971d02ea6a07389e671ce51c8b9e
Ricardo Rodrigues <originalrcd@hotmail.com>
2024-06-12 10:17:00 -0300

    heroku_issue2

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,15 +1,13 @@
 Geocoder.configure(
   # Geocoding options
-
-  # Geocoder.configure(http_headers: { "User-Agent" => "your contact info" })
   timeout: 10,                 # geocoding service timeout (secs)
-  # lookup: :nominatim,         # name of geocoding service (symbol)
-   # name of IP address geocoding service (symbol)
+  lookup: :mapbox,         # name of geocoding service (symbol)
+  # ip_lookup: :telize,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: ENV['MAPBOX_API_KEY'],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
 
   # Exceptions that should not be rescued by default
