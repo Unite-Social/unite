b6d0b2a3cbb43529b27cd9ac18c36a778fbb542d
felipe imbimbo <f_imbimbo@hotmail.com>
2024-06-17 15:44:08 -0300

    dashboard done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @movements_per_user = Movement.group(:user_id).count
+    @participants_per_movement = Participant.group(:movement_id).count
   end
 end
